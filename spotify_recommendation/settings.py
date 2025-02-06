4b2c31086ded9f55c03c0e1378e63ddbe340b581
tab1shh <164711380+tab1shh@users.noreply.github.com>
2025-02-05 20:31:47 -0500

    Fixed dj-database-url version for Heroku

--- spotify_recommendation/settings.py
+++ spotify_recommendation/settings.py
@@ -77,11 +77,10 @@ WSGI_APPLICATION = "spotify_recommendation.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 

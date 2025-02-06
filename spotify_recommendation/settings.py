5013e3ed5b2d2914de18003eb8bb1403363d120e
tab1shh <164711380+tab1shh@users.noreply.github.com>
2025-02-05 21:20:45 -0500

    Update ALLOWED_HOSTS for Heroku

--- spotify_recommendation/settings.py
+++ spotify_recommendation/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_Secret_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['gentle-inlet-29851.herokuapp.com']
+ALLOWED_HOSTS = ['gentle-inlet-29851.herokuapp.com', "localhost", "127.0.0.1"]
 
 
 # Application definition

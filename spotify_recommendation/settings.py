499f1f9cfb898fc048e06f00cbc7c4f3e3869f53
tab1shh <164711380+tab1shh@users.noreply.github.com>
2025-02-05 21:55:20 -0500

    databses

--- spotify_recommendation/settings.py
+++ spotify_recommendation/settings.py
@@ -77,11 +77,9 @@ WSGI_APPLICATION = "spotify_recommendation.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
